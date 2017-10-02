acb83a3a318c9a4b273bdb384c346ef6aefa1397
Dan Spector <spector@fastmail.com>
2017-10-02 17:23:19 -0400

    "js commit 3"

---
+++ _site/js_1/js/script.js
@@ -0,0 +1,9 @@
+function magicText(adjective) {
+  var text = 'This ' + adjective + ' text is magic';
+  document.getElementById("magic").innerHTML = text;
+  // return text;
+}
+//
+// function changeInnerHTML(text) {
+//   document.getElementById("magic").innerHTML = text;
+// }
