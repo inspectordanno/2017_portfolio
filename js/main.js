cb2e59d7f10dc3934d2d57b0a5dc027cd677a532
Dan Spector <danno2423@gmail.com>
2017-12-29 14:59:47 -0500

    "kinda fixed navbar collapse"

--- js/main.js
+++ js/main.js
@@ -10,12 +10,10 @@ $(document).ready(function() {
 
 
 		//collapse mobile navbar
-    $(document).click(function (event) {
-        var clickover = $(event.target);
-        var _opened = $(".navbar-collapse").hasClass("navbar-collapse in");
-        if (_opened === true && !clickover.hasClass("navbar-toggle")) {
-            $("button.navbar-toggle").click();
-        }
-    });
+		$(document).on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a') ) {
+        $(this).collapse('hide');
+    }
+		});	
 
 });
